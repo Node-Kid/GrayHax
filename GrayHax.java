c42c5dedd1fca14a4c5eedf0cc0620c543d0dbc8
Will W <weiszhaarwill@gmail.com>
2021-10-30 21:45:44 -0500

    Adding modulemanager

--- GrayHax.java
+++ GrayHax.java
@@ -1,5 +1,17 @@
 package com.grayjam.grayhax.GrayHax;
 
+import org.lwjgl.opengl.Display;
+
+import com.grayjam.grayhax.GrayHax.modules.ModuleManager;
+
 public class GrayHax {
-	public int ver = 1;
+	public static String name = "GrayHax", version = "0.1.0", creator = "GrayJam";
+	public static ModuleManager moduleManager;
+	public void startClient() {
+		moduleManager = new ModuleManager();
+		
+		Display.setTitle(name + " " + version + "by " + creator);
+		
+	}
+	
 }
--- modules/ModuleManager.java
+++ modules/ModuleManager.java
@@ -1,5 +1,30 @@
 package com.grayjam.grayhax.GrayHax.modules;
 
-public class ModuleManager {
+import java.util.ArrayList;
 
+public class ModuleManager {
+	private static ArrayList<Module> mods;
+	public ModuleManager() {
+		mods = new ArrayList<Module>();
+		//combat
+		
+		//movement
+		
+		//player
+		
+		//render
+		
+		//misc
+	}
+	public static void newMod(Module m)  {
+		mods.add(m);
+	}
+	public static ArrayList<Module> getModules() {
+		return mods;
+	}
+	public static void onUpdate() {
+		for(Module m : mods) {
+			m.onUpdate();
+		}
+	}
 }
